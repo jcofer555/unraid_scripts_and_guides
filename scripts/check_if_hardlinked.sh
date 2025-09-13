f68b8299ccd8b82c1d6a473753658c0ee757378b
jcofer555 <140566618+jcofer555@users.noreply.github.com>
2025-09-12 17:19:40 -0700

    Update check_if_hardlinked.sh

--- scripts/check_if_hardlinked.sh
+++ scripts/check_if_hardlinked.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$(stat -c '%i' /mnt/user/backups/mymedia/downloads/complete/tv/Hawaii.Five-O.S12.1080p.AMZN.WEB-DL.DDP2.0.H.264-MZABI/Hawaii.Five-O.S12E20.Woe.To.Wo.Fat.1080p.AMZN.WEB-DL.DDP2.0.H.264-MZABI.mkv)" -eq "$(stat -c '%i' /mnt/user/backups/mymedia/downloads/tv/Hawaii\ Five-O\ 1968/Hawaii\ Five-O\ 1968\ -\ Season\ 12/Hawaii\ Five-O\ 1968\ -\ S12E20\ -\ Woe\ To\ Wo\ Fat.mkv)" ]; then
+if [ "$(stat -c '%i' /mnt/user/backups/mymedia/downloads/complete/tv/episode.mkv)" -eq "$(stat -c '%i' /mnt/user/backups/mymedia/downloads/tv/episode.mkv)" ]; then
     echo "The files are hardlinked."
 else
     echo "The files are not hardlinked."
